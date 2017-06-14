31e9fe5b81307860f0bd6089df4dae74340c8e14
pinocchio <wkim149@gmail.com>
2017-06-14 00:46:53 -0400

    add connect_four_refree in ruby

---
+++ connect_four/board.rb
@@ -0,0 +1,86 @@
+module ConnectFour
+
+  class Board
+    attr_reader :draw, :last
+
+    def initialize(w, h)
+      @w, @h = w, h
+
+      @draw = false
+      @piece_cnt = 0
+      @piece_max = @w * @h
+      @board = h.times.map do |_|
+        w.times.map { |_| nil }
+      end
+    end
+
+    def get
+      @board.map(&:clone)
+    end
+
+    def contains?(r, c)
+      r.between?(0, @h - 1) and c.between?(0, @w - 1)
+    end
+
+    def floating?(r, c)
+      @board[(r + 1)..-1].any? { |x| x[c].nil? }
+    end
+
+    def play(spot, piece)
+      if not spot.nil? then
+        r, c = spot
+        # within the board?
+        spot = nil if not self.contains?(r, c)
+        # already played? or floating?
+        spot = nil if not @board[r][c].nil? or self.floating?(r, c)
+      end
+
+      @draw = true if @last.nil? and spot.nil?
+      @last = spot
+      return if spot.nil?
+
+      r, c = spot
+      @board[r][c] = piece
+
+      @piece_cnt += 1
+      draw = true if @piece_cnt == @piece_max # truly true only if gameover? is false
+    end
+
+    def gameover?
+      return false if @last.nil?
+      # only need to check the last move
+      return self.lines(@last).any? { |l| self.four? l }
+    end
+
+    def lines(spot)
+      r, c = spot
+      # -, |, \, /
+      return [@board[r][0..-1],
+              @board[0..-1].map { |r| r[c] },
+              self.diagonal(c - r),
+              self.diagonal(c + r, true)]
+    end
+
+    # \ (default)
+    def diagonal(i, reverse=false)
+      row_len = @board[0].length - 1
+      col_range = if reverse then ((i - row_len)..i).reverse_each else (i..(i + row_len)) end
+
+      spots = (0...@board.length).zip col_range
+      return spots.map do |r, c|
+        next if not c.between?(0, row_len)
+        @board[r][c]
+      end
+    end
+
+    def four?(ln)
+      ln.chunk_while { |a, b| a == b }.each do |c|
+        next if c[0].nil?
+        return true if c.length == 4
+      end
+      return false
+    end
+
+  end
+
+end
---
+++ connect_four_refree.rb
@@ -0,0 +1,48 @@
+require_relative 'parkutil'
+require_relative 'connect_four'
+
+class ConnectFourRefree
+
+  def initialize(w, h)
+    Parkutil.register_function('play', 3)
+    Parkutil.load_players(2)
+
+    @board = ConnectFour::Board.new(w, h)
+  end
+
+  def start
+    while true do
+      self.over!
+      self.playturn
+    end
+  end
+
+  def over!
+    # check myself if lost
+    if @board.gameover? then
+      puts Parkutil.player(Parkutil.turn(-1)).uid
+      exit 0
+    end
+
+    # draw
+    exit 1 if @board.draw
+  end
+
+  def playturn
+    spot = nil
+    begin
+      spot = Parkutil.current_player.play(@board.get, @board.last, Parkutil.turn)
+    rescue Parkutil::ClockTimeout
+    rescue => e
+      puts Parkutil.current_player.uid
+      Parkutil.print_exception(e)
+      exit 2
+    end
+
+    @board.play(spot, Parkutil.turn)
+    Parkutil.advance_turn
+  end
+
+end
+
+ConnectFourRefree.new(7, 6).start
