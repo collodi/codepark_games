78a437694b7181cb812eccea83220a7a519313b1
pinocchio <wkim149@gmail.com>
2017-06-13 21:01:53 -0400

    remove connect four python

--- connect_four/players/tester1.py
+++
@@ -1,6 +0,0 @@
-def play(board, lastspot, mylast, piece, retry):
-    for c in range(len(board[0])):
-        for r in range(len(board)-1, -1, -1):
-            if board[r][c] == None:
-                return (r, c)
-    return (None, None)
